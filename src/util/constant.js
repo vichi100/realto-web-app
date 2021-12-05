f19fc64d5937130cfce080a3f1053aeaf8e779e6
vichi100 <vichi100@gmail.com>
2021-12-05 18:57:34 +0530

    url change

--- src/util/constant.js
+++ src/util/constant.js
@@ -1,6 +1,6 @@
 
-export const SERVER_URL_DEV = "http://192.168.1.104:7000";
-export const SERVER_URL_PROD = "http://209.145.57.26:7000";
+export const SERVER_URL_DEV = "http://192.168.0.100:7000";
+export const SERVER_URL_PROD = "http://66.94.118.23:7000";
 export const SERVER_URL = SERVER_URL_PROD;
 
 // export const SERVER_URL = "http://192.168.1.104:7000";
@@ -8,3 +8,6 @@ export const SERVER_URL = SERVER_URL_PROD;
 
 
 // export const SERVER_URL = "http://192.168.0.100:3000/";// Prod
+
+
+
